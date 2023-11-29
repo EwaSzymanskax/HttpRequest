4b8635f8ab1d3d7faa18f5a314afcec266b21b5d
Ewa Szymańska <ewa.szymanskax@gmail.com>
2023-11-29 12:31:57 +0100

    Post request

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,20 +1,45 @@
 const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
 
-const xhr = new XMLHttpRequest();
+function sendHttpRequest(method, url, data){
+const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
 
- 
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+    xhr.open(method, url);
 
-xhr.onload = function() {
-  const listOfPosts = JSON.parse(xhr.response);
-  for (const post of listOfPosts){
-     const postEl = document.importNode(postTemplate.content,  true);
-     postEl.querySelector('h2').textContent = post.title.toUpperCase();
-     postEl.querySelector('p').textContent = post.body;
-     listElement.append(postEl);
+    xhr.onload = function() {
+    resolve(xhr.response);
+     
+    };
+    xhr.send(JSON.stringify(data)); 
+})
+return promise; 
+}
 
-  }
-};
+async function fetchPosts(){
+    const responseData = await sendHttpRequest('GET', 'https://jsonplaceholder.typicode.com/posts')
 
-xhr.send(); 
+    const listOfPosts = JSON.parse( responseData);
+    for (const post of listOfPosts){
+    const postEl = document.importNode(postTemplate.content,  true);
+    postEl.querySelector('h2').textContent = post.title.toUpperCase();
+    postEl.querySelector('p').textContent = post.body;
+    listElement.append(postEl);
+    };
+           
+}
+
+async function createPosts(title, content) {
+    const userId = Math.random();
+    const post = {
+        title: title,
+        body: content,
+        userId: userId,
+    }
+    sendHttpRequest('POST', 'https://jsonplaceholder.typicode.com/posts', post)
+}
+
+
+
+fetchPosts();
+createPosts('DUMMY', 'A dummy post!')
